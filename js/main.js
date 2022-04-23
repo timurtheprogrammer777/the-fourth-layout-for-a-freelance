f02b5f5066a3bd9691e6a81b8584821b987a6f0e
timur1243 <86830091+timur1243@users.noreply.github.com>
2022-04-23 15:03:01 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+$(function () {
+
+
+    $('.team__inner-content').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: false,
+      dots: true,
+    //   autoplay: true,
+    //   autoplaySpeed: 1500,
+      
+  
+    });
+
+//     $('.team__inner-content').slick({
+//       infinite: true,
+//       slidesToShow: 1,
+//       slidesToScroll: 1,
+//       arrows: true,
+//       dots: true,
+//       autoplay: true,
+//       autoplaySpeed: 1500,
+      
+  
+//     });
+
+//     $('.clients__inner-content').slick({
+//       infinite: true,
+//       slidesToShow: 1,
+//       slidesToScroll: 1,
+//       arrows: true,
+//       dots: true,
+//       autoplay: true,
+//       autoplaySpeed: 1500,
+      
+  
+//     });
+
+
+//     $('.burger-btn').on('click', function(){
+//       $('.header-ul').slideToggle(); 
+//   });
+  
+  
+  
+  
+  });
